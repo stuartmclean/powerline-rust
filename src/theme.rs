d983e87a3ac6fdc3e8500b9948ed18abbd8ecff9
Stuart McLean <stuart.mclean@soundcloud.com>
2024-04-12 04:53:17 +0200

    use nicer color for dirty git branch

--- src/theme.rs
+++ src/theme.rs
@@ -57,7 +57,7 @@ impl GitScheme for SimpleTheme {
     const GIT_NOTSTAGED_FG: Color = Color(15);
     const GIT_REPO_CLEAN_BG: Color = Color(148);
     const GIT_REPO_CLEAN_FG: Color = Color(0);
-    const GIT_REPO_DIRTY_BG: Color = Color(160);
+    const GIT_REPO_DIRTY_BG: Color = Color(130);
     const GIT_REPO_DIRTY_FG: Color = Color(15);
     const GIT_STAGED_BG: Color = Color(22);
     const GIT_STAGED_FG: Color = Color(15);
